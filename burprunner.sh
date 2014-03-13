6c399b6d35774e4e6d37d48daee761e2d0725dc7
Soroush Dalili <soroush.dalili@gmail.com>
2014-03-13 13:25:50 +0000

    solving ssl problems
    optional "-Djsse.enableSNIExtension=false" was added to solve some ssl problems

--- burprunner.sh
+++ burprunner.sh
@@ -1,3 +1,6 @@
 # Author: @irsdl
 # 1GB memory - you can change it
 java -Xmx1024m -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
+
+# if you had ssl problem, use this:
+# java -Xmx1024m -Djsse.enableSNIExtension=false -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
