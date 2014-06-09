7414018c8540fc8a31d2e0e53bfedfdb5fa7a9e3
Soroush Dalili <soroush.dalili@gmail.com>
2014-06-09 21:30:30 +0100

    perm size
    perm size

--- burprunner.sh
+++ burprunner.sh
@@ -1,6 +1,6 @@
 # Author: @irsdl
-# 1GB memory - you can change it
-java -Xmx1024m -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
+# 1GB memory, 256MB perm size - you can change them - you can change it
+java -Xmx1024m -XX:MaxPermSize=256m -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
 
 # if you had ssl problem, use this:
-# java -Xmx1024m -Djsse.enableSNIExtension=false -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
+# java -Xmx1024m -XX:MaxPermSize=512m -Djsse.enableSNIExtension=false -jar $(ls -t $(dirname $(readlink -f ${BASH_SOURCE[0]}))/burpsuite*.jar|head -1)
